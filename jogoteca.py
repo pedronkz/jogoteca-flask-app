94c3e3df66e1ad19b7f49e2d66d33c8dc172970e
Pedro Ferreira <pedroholiveiraferreira27@gmail.com>
2026-01-21 13:23:54 -0300

    Refactor to OOP, add POST forms and inheritance
    Data Structure: Implemented the Jogo class to encapsulate name, category, and console attributes.
    
    Game Creation: Added the /novo route for the entry form and a /criar route to process submissions.
    
    Security & Logic: Configured the POST method and implemented redirect(url_for('index')) to prevent form resubmission.
    
    UI/UX: Integrated Bootstrap via a static folder and applied Template Inheritance to eliminate redundant HTML code.
    
    Dev Workflow: Enabled Flask Debug Mode and standardized the root route to / with the index() function.

--- jogoteca.py
+++ jogoteca.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-# 1. Cria a instância do Flask (o 'coração' do site)
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
+jogo3 = Jogo('Skyrim', 'RPG', 'PC')
+lista = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
-# 2. Define a rota (endereço URL) e a função (o que o site faz)
-@app.route('/inicio')
-def ola():
-    # Retorna HTML para o navegador
-    return render_template('lista.html')
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-# 3. Executa o servidor (Modo Debug ativado para facilitar)
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    novo_jogo = Jogo(nome, categoria, console)
+    lista.append(novo_jogo)
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
